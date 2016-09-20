91349ef1fbf0d7788174643e25c05f12f4295f4a
srsutherland <SeanRSuther@gmail.com>
2016-09-20 16:50:51 -0700

    Converted to typescript

--- canvasPolar.ts
+++ canvasPolar.ts
@@ -1,45 +1,49 @@
-var CMint = '#3f9', CPurp = '#93f', CBlue = '#39f';
-var CLime = '#9f3', COran = '#f39', CPink = '#f93';
-var CAry = [CMint, CPurp, CBlue, CLime, COran, CPink];
-var elem = document.getElementById("canvas");
-var canvasPolar;
+const CMint = '#3f9', CPurp = '#93f', CBlue = '#39f';
+const CLime = '#9f3', COran = '#f39', CPink = '#f93';
+const CAry = [CMint, CPurp, CBlue, CLime, COran, CPink];
+
+let elem = document.getElementById("canvas");
+let canvasPolar : HTMLCanvasElement;
 if (elem instanceof HTMLCanvasElement) {
     canvasPolar = elem;
-}
-else {
+} else {
     canvasPolar = new HTMLCanvasElement;
     elem.appendChild(canvasPolar);
 }
-var ctx = canvasPolar.getContext("2d");
-var radius = canvasPolar.height / 2;
+
+let ctx = canvasPolar.getContext("2d");
+let radius = canvasPolar.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90;
-var theta = 0;
-var params = genAlpha();
+let theta = 0;
+let params = genAlpha();
 ctx.fillStyle = params.color;
-var clearI = setInterval(drawGraph, 1);
+let clearI = setInterval(drawGraph, 1);
+
 function drawGraph() {
     if (theta >= params.max) {
         params = genAlpha();
         ctx.fillStyle = params.color;
         theta = 0;
     }
-    var step = Math.PI / 256 / 2;
+    var step = Math.PI / 256 /2;
     drawPoint(ctx, radius, theta, params.A, params.B);
     theta += step;
-    ctx.rotate(step);
+    ctx.rotate(step)
 }
+
 function genAlpha() {
-    var rVal = {};
+    let rVal : any = {};
     rVal.color = CAry[getRandomInt(0, 5)];
     rVal.A = getRandomInt(1, 20);
     rVal.B = getRandomInt(1, 20);
-    var aGCD = gcd(rVal.A, rVal.B);
+    let aGCD = gcd(rVal.A, rVal.B);
     rVal.A /= aGCD;
     rVal.B /= aGCD;
-    rVal.max = rVal.B * Math.PI * (rVal.A % 2 == 0 || rVal.B % 2 == 0 ? 2 : 1) + (Math.PI / 2);
+    rVal.max = rVal.B * Math.PI * (rVal.A % 2 == 0 || rVal.B % 2 == 0 ? 2 : 1) + (Math.PI/2);
     return rVal;
 }
+
 //noinspection JSUnusedGlobalSymbols
 function initParams() {
     return {
@@ -47,33 +51,27 @@ function initParams() {
         A: 19,
         B: 6,
         max: 6 * 2 * Math.PI
-    };
+    }
 }
+
 function drawPoint(ctx, radius, theta, A, B) {
     ctx.beginPath();
     ctx.arc(0, -radius * Math.sin(A * theta / B), radius * .01, 0, 2 * Math.PI, false);
-    ctx.fill();
+    ctx.fill()
 }
-function gcd(a, b) {
-    if (a < 0)
-        a = -a;
-    if (b < 0)
-        b = -b;
-    if (b > a) {
-        var temp = a;
-        a = b;
-        b = temp;
-    }
+
+function gcd(a,b) : number {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (b > a) {var temp = a; a = b; b = temp;}
     while (true) {
-        if (b == 0)
-            return a;
+        if (b == 0) return a;
         a %= b;
-        if (a == 0)
-            return b;
+        if (a == 0) return b;
         b %= a;
     }
 }
-function getRandomInt(min, max) {
+
+function getRandomInt(min, max) : number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//# sourceMappingURL=canvasPolar.js.map
